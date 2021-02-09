32ee10bbc073f780e60322cc4c4402540053e863
Ivan Lukyanau <51121952+Lukyanau@users.noreply.github.com>
2021-02-09 17:53:05 +0300

    Update Runner.java

--- Task3Git/src/main/java/by/epam_training/lukyanau/task3/Runner.java
+++ Task3Git/src/main/java/by/epam_training/lukyanau/task3/Runner.java
@@ -2,7 +2,7 @@ package by.epam_training.lukyanau.task3;
 
 import by.epam_training.lukyanau.task3.exception.ProjectException;
 import by.epam_training.lukyanau.task3.printer.impl.PrinterImpl;
-import by.epam_training.lukyanau.task3.reader.XmlReader;
+import by.epam_training.lukyanau.task3.reader.XmlFileReader;
 import by.epam_training.lukyanau.task3.service.impl.XmlServiceImpl;
 
 
@@ -11,7 +11,7 @@ public class Runner {
     public static final String PATH = "resources/xmlTest.xml";
 
     public static void main(String[] args) throws ProjectException {
-        XmlReader reader = new XmlReader();
+        XmlFileReader reader = new XmlFileReader();
         XmlServiceImpl xmlService = XmlServiceImpl.getInstance();
         PrinterImpl printer = PrinterImpl.getInstance();
         printer.printInfo(xmlService.createTreeStructure(reader.getFileData(PATH)));
